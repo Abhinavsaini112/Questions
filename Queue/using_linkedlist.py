1407841dde90800545e3761e3b2409dc21b7c884
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2025-03-04 00:48:20 +0530

    queue linked list

--- Queue/using_linkedlist.py
+++ Queue/using_linkedlist.py
@@ -12,38 +12,32 @@ class QueueUsingLinkedlist:
     def enqueue(self,element):
         newNode=Node(element)
         if self.__head is None:
-            self.__head=newNode
-            
+            self.__head=newNode   
         else:
-            self.__tail.next=newNode
-            
+            self.__tail.next=newNode   
         self.__tail=newNode
-        self.__count=self.__count+1
+        self.__count+=1
         
     def dequeue(self):
         if self.__head is None:
-            print("Hey!Queue is Empty")
-            return
+            return "Hey!Queue is Empty"
         data=self.__head.data
         self.__head=self.__head.next
-        self.__count=self.__count-1
+        self.__count-=1
         return data
-        
-            
-        
+      
     def isEmpty(self):
-        return self.size()==0
-        
-        
+        return self.__count==0
+           
     def size(self):
         return self.__count
         
     def front(self):
-        if self.__head is None:
-            print("Hey! Queue is Empty")
-            return
-        data=self.__head.data
-        return data
+        if self.isEmpty():
+            return "Hey! Queue is Empty"
+        else:
+            return self.__head.data
+
 
 q=QueueUsingLinkedlist()
 q.enqueue(1)
